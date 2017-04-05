c9928910d550a89f67b4effc93a981a0a3ce8f99
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-04-05 09:59:18 +0900

    userモデルのnameに対して、uniquenessを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
 end
