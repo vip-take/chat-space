5fe163c8ea82cc44c44c04683f855088fc01d3c8
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-04-07 20:28:13 +0900

    userモデルに、messagesのhas_manyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :groups, through: :group_users
   has_many :group_users, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 
