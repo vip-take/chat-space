70d25d1c9a0e0ac3767147accf288deef6322f96
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-04-12 13:01:01 +0900

    グループ作成の際に誰かしらメンバーがいないと登録できないようにバリデーション設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,16 +1,15 @@
 class Group < ApplicationRecord
 
-  # TODO:グループメンバーのチェックを入れる
-  # before_save :group_members_present?
+  before_validation :group_members_present?
 
   has_many :users, through: :group_users
   has_many :group_users, dependent: :destroy
   has_many :messages, dependent: :destroy
 
-  validates :name, presence: true, null: false,  uniqueness: true
+  validates :name, presence: true, null: false, uniqueness: true
 
   def group_members_present?
-    # self.user_ids.present? && self.user_ids.include?(current_user.id)
+    self.user_ids.present?
   end
 
 end
