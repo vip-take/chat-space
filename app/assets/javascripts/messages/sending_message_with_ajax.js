25fad0ac14a2657a6e5d337664631ff25fb45828
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-04-12 10:06:19 +0900

    message送信用のjsにコメントや画像が空の場合の処理を追加

--- app/assets/javascripts/messages/sending_message_with_ajax.js
+++ app/assets/javascripts/messages/sending_message_with_ajax.js
@@ -4,6 +4,8 @@ $(function() {
     var form = $('#new_message').get()[0];
     var formData = new FormData(form);
     var postUrl = $('.new_message').attr('action')
+    console.log(formData);
+
     $.ajax({
       type: 'POST',
       url: postUrl,
@@ -15,14 +17,17 @@ $(function() {
     .done(function(data){
       var addUser = "<div class='timeline-message'><div class='timeline-message__user'><div class='timeline-message__user__name'>" + data.name + " </div><div class='timeline-message__user__date'>" + data.created_at + "</div></div></div>";
       $('.timeline').append(addUser);
-      // TODO:後で分岐挿入
-      var addText = "<div class='timeline-message__text'>" + data.comment + "</div>";
-      $('.timeline .timeline-message:last-child').append(addText);
 
-      // TODO:後で分岐挿入
+      if (data.comment.length != 0){
+        var addText = "<div class='timeline-message__text'>" + data.comment + "</div>";
+        $('.timeline .timeline-message:last-child').append(addText);
+      }
+
+      if (data.image.url != null){
       var addImage = "<img class='timeline-message__image' src='" + data.image.url + "' alt='Pylamid'>";
-
       $('.timeline .timeline-message:last-child').append(addImage);
+      }
+
       $('#js-comment-input').val('');
       $('#js-file-input').val('');
     })
