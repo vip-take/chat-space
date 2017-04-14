8bb537aecbcf3b798368ddb5ed3c7cf088bcdb5e
Shuhei Taketani <shuhei.taketani@gmail.com>
2017-04-14 15:57:44 +0900

    capistra noの設定値の再調整

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :rbenv_ruby, '2.3.1'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/staketani/.ssh/tc-chatspace-01.pem']
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_pid, -> { "#{deploy_to}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 
 # Default value for keep_releases is 5
